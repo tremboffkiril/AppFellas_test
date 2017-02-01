40b9e3c2399465abf875498a0e4b1af76e5fc805
Kiril Trembovetskiy <tremboffkirya96@gmail.com>
2017-02-02 00:11:57 +0200

    first commit

---
+++ AppFellas_Test/LogOutCell.swift
@@ -0,0 +1,24 @@
+//
+//  LogOutCell.swift
+//  AppFellas_Test
+//
+//  Created by Kiril Trembovetskiy on 2/1/17.
+//  Copyright © 2017 Kiril Trembovetskiy. All rights reserved.
+//
+
+import UIKit
+
+class LogOutCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AppFellas_Test/MyAccountCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyAccountCell.swift
+//  AppFellas_Test
+//
+//  Created by Kiril Trembovetskiy on 2/1/17.
+//  Copyright © 2017 Kiril Trembovetskiy. All rights reserved.
+//
+
+import UIKit
+
+class MyAccountCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
